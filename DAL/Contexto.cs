4394a09da3cd2c32a99476efd929a56bed424d00
Alvin <alvindejesus88@gmail.com>
2020-11-16 22:10:16 -0400

    Segunda commit

---
+++ DAL/Contexto.cs
@@ -0,0 +1,24 @@
+﻿using Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Usuarios> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+
+}
---
+++ Entidades/Usuarios.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entidades
+{
+    public class Usuarios
+    {
+        public int UsuarioId { get; set; }
+        public int Clave { get; set; }
+        public int Nombres { get; set; }
+    }
+}
