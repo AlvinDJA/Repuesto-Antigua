03335f4d98300a05fa3d8fa21d5ccadde155174e
Alvin <alvindejesus88@gmail.com>
2020-11-20 14:15:48 -0400

    BLL, DAL, Entidades

---
+++ RepuestoAntigua/UI/Login.xaml.cs
@@ -0,0 +1,43 @@
+﻿using BLL;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+namespace RepuestoAntigua.UI
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        private void AccederBoton_Click(object sender, RoutedEventArgs e)
+        {
+            Acceder();
+        }
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Acceder();
+            }
+        }
+        private void Acceder()
+        {
+            string nombre = NombreUsuarioTextBox.Text.Trim();
+            string pass = PasswordBox.Password;
+
+            if (UsuariosBLL.Validar(nombre, pass))
+            {
+                new MainWindow().Show();
+                this.Close();
+            }
+            else
+            {
+                ValidacionIncorrectaLabel.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
