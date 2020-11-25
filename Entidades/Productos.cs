d6261a64dbaec49c33d0a00c3a2c8692bd6521bd
Alvin <alvindejesus88@gmail.com>
2020-11-25 11:28:49 -0400

    Agregando Ventanana Registro Productos

--- Entidades/Productos.cs
+++ Entidades/Productos.cs
@@ -7,10 +7,10 @@ namespace Entidades
         [Key]
         public int ProductoId { get; set; }
         public int UsuarioId { get; set; }
-        public int MarcaId { get; set; }
+        public int MarcaId { get; set; }//
         public string Descripcion { get; set; }
         public float Cantidad { get; set; }
-        public int NoSerie { get; set; }
+        public int NoSerie { get; set; }//
         public float Precio { get; set; }
         public float Costo { get; set; }
         public float PorcentajeITBIS { get; set; }
